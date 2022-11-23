accd2355985c0306a147e0be16e94007d314c817
dedperdos <91718475+dedperdos@users.noreply.github.com>
2022-11-23 21:43:08 +0200

    Add files via upload

--- 31.10-06.11 №2.cpp
+++ 31.10-06.11 №2.cpp
@@ -1,30 +1,46 @@
 ﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
-
-
-
 int main() {
-	int temp;
-	int a[5];
-	for (int i = 0; i < 5; i++) {
-		cin >> a[i];
-	}
-	for (int i = 0; i < 5; i++) {
-		if (a[i] < 0)
-			a[i] = 0;
-	}
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			if (a[j] > a[j + 1]) {
-				temp = a[j];
-				a[j] = a[j + 1];
-				a[j + 1] = temp;
-			}
-		}
-	}
-	for (int i = 0; i < 5; i++) {
-		cout << a[i] << ' ';
-	}
+    int N = 10;
+    int max = 0;
+    int array[N];
 
+    srand(time(NULL));
+    cout << "Вхідний масив:" << endl;
+    for (int i = 0; i < N; i++) {
+        array[i] = rand() % 20 - 10;
+        if (i == N - 1) {
+            cout << array[i] << endl;
+        }
+        else {
+            cout << array[i] << ", ";
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        if (array[i] == max) {
+            max = i;
+        }
+    }
+    for (int i = 0; i < max; i++) {
+        if (array[i] < 0) {
+            array[i] = 0;
+        }
+    }
+    cout << "Вихідний масив:" << endl;
+    for (int i = 0; i < N; i++) {
+        if (i == N - 1) {
+            cout << array[i] << endl;
+        }
+        else {
+            cout << array[i] << ", ";
+        }
+    }
 }
